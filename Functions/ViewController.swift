b520d9ce48b0e889f102919da42e764668c71f23
Emerson <emersonmixrouse@gmail.com>
2023-03-01 10:43:02 -0700

    MVP

--- Functions/ViewController.swift
+++ Functions/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let sumOfNumbersA:Int = 0
+    let sumOfNumbersB:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        sayHello()
+        sayHelloToMeWithName(name: "Emerson")
+        sayHelloToMeWithFullName (first:"Emerson", last:"Rouse")
+        labelLearning(parameterName: 4)
+        labelLearning(PleaseEnterNumber: 3)
+       // labelLearning(6)
+        sumOfNumbersA(x:5, y:3)
+        sumOfNumbersB(x:5, y:3)
+        swapPlaces(left: 5, right: 3)
+        let myTuple = swapPlaces(left:5, right:3)
+        
+        
+        
+        
+        
+    func sayHelloToMeWithFullName (first:String, last:String) {
+        let result = "Hello " + first + " "+last
+        print(result)
     }
-
-
+    
+    func sayHelloToMeWithName (name: String) {
+        let result = "Hello \(name)"
+        print(result)
+    }
+    func sayHello(){print ("Hello")}
+    
+    func labelLearning(parameterName: Int) {}
+    
+    func labelLearning(PleaseEnterNumber parameterName: Int) {}
+   // labelLearning(PleaseEnterNumber: 3)
+    
+    func labelLearning(_parameterName: Int) {}
+   // labelLearning(6)
+    
+    func sumOfNumbersA(x:Double, y:Double) -> Double {
+        let result = x + y
+        return result
+    }
+   // sumOfNumbersA(x:5, y:3)
+    
+    func sumOfNumbersB(x: Double, y: Double) -> Double {
+        return x + y
+    }
+   // sumOfNumbersB(x:5, y:3)
+    
+    func swapPlaces(left:Int, right:Int) -> (Int, Int) {
+        return (right, left)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
